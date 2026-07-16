e3df37e8ff6206c8da1587f53312da5d720a8433
deepika seerapu <deepika.seerapu24@sasi.ac.in>
2026-07-16 19:05:28 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://chatapp-backend-csmc-deepika.onrender.com",
+  baseURL: "https://chatapp-backend-csmc-deepika-1.onrender.com",
 });
 export default API;
